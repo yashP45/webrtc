1fcb3b18f8b7c1a74d07ea053e49f93886d912b9
yash <yashporwal208@gmail.com>
2023-12-13 18:12:52 +0530

    new

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import react from "@vitejs/plugin-react";
-import 'vite/modulepreload-polyfill'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
